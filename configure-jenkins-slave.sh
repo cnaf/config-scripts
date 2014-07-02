a8b88788fa54b70d0e10edcd51646969931df457
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-07-02 15:48:59 +0200

    documentation and indentation
    More informational comments on maven server configuration.

--- configure-jenkins-slave.sh
+++ configure-jenkins-slave.sh
@@ -2,7 +2,11 @@
 
 modules_dir="/etc/puppet/modules"
 manifest="jenkins-manifest.pp"
-maven_file="maven_settings.cvs"
+
+## Path to a comma-separated-value file describing known maven repositories 
+## and containing lines in the following format:
+## server_id,login,password
+maven_server_conf="maven_server_conf.csv"
 
 echo "Creating manifest file for jenkins-slave setup"
 
@@ -11,7 +15,7 @@ servers_params=""
 #Set the internal field separator to ","
 OLDIFS=$IFS
 IFS=,
-if [ -f $maven_file ] && [ ! -z $maven_file ]; then
+if [ -f $maven_server_conf ] && [ ! -z $maven_server_conf ]; then
 
   while read id login pwd
   do
@@ -28,30 +32,31 @@ class { 'puppet-jenkins-slave':
         maven_servers_data => [ $servers_params ]
 }
 
+include puppet-users
 include puppet-storm-build-deps
 include puppet-voms-build-deps
 include puppet-robot-framework
 EOF
 
-echo "Installing custom puppet modules"
+echo "Installing base puppet modules"
 
-# install custom puppet modules
 puppet module install --force maestrodev-wget
 puppet module install --force gini-archive
 puppet module install --force puppetlabs-stdlib
 puppet module install --force maestrodev-maven
 puppet module install --force puppetlabs-java
 
-# Download all puppet modules from CNAF organization
-
+# Fetch all puppet modules from CNAF organization
 echo "Fetching puppet modules from: https://github.com/cnaf"
 
 list=$(curl https://api.github.com/orgs/cnaf/repos|grep html_url|sed  's/[",]//g'|sed -rn  's/.+(https.+(puppet))/\1/p'|sed  's/https/git/g')
-for url in $list;do repo=$(echo $url|sed -rn  's/(^git.+(puppet))/\2/p' );git clone $url $modules_dir/$repo;done
 
-echo "Applying puppet manifest: $manifest"
+for url in $list; do 
+  repo=$(echo $url|sed -rn  's/(^git.+(puppet))/\2/p' )
+  git clone $url $modules_dir/$repo;
+done
+
+echo "Applying the following puppet manifest: $manifest"
 cat $manifest
 
 puppet apply --color false --debug -v $manifest
-
-
